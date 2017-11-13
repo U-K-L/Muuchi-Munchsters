6af9443bdeec381b3f83b695a95c37c77ce41872
UKL <WilliamsNoah1720@gmail.com>
2017-11-12 19:02:28 -0500

    Initializing Full Devolpment enviroment

---
+++ Muuchi Munchy/desktop/src/com/muuchi/game/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.muuchi.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.muuchi.game.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width=1920;
+		config.height=1080;
+		config.title = "Muuchi Munchy";
+		new LwjglApplication(new Game(), config);
+	}
+}
